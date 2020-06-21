482f2f94d80170e843c0ecdd12d6df32f67bc77f
Jan Martens <jan.niklas.martens@mni.thm.de>
2020-06-21 21:33:57 +0200

    - Rework search result to implement dynamic generation - Init Shipment class - Init DeliveryObject interface

---
+++ src/app/search-result/list-result/list-result.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-result',
+  templateUrl: './list-result.page.html',
+  styleUrls: ['./list-result.page.scss'],
+})
+export class ListResultPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
