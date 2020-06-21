9e6cf036f08cd0f53e29886fe28f1d23a75346f1
Johannes Greising <johannes.greising@mni.thm.de>
2020-06-21 20:41:13 +0200

    Added a star rating prototype

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -1,14 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
   styleUrls: ['./rating.component.scss'],
 })
+
+
 export class RatingComponent implements OnInit {
+  @Input() rating: number;
+  @Output() ratingChange: EventEmitter<number> = new EventEmitter();
 
   constructor() { }
 
+  rate(index: number){
+    this.rating = index;
+    this.ratingChange.emit(this.rating);
+  }
+
+  getColor(index: number){
+    if (this.isAboveRating(index)){
+      return COLORS.GREY;
+    }
+    switch(this.rating){
+      case 1:
+      case 2:
+        return COLORS.RED;
+      case 3:
+        return COLORS.YELLOW;
+      case 4:
+      case 5:
+        return COLORS.GREEN;
+      default:
+        return COLORS.GREY;
+    }
+  }
+
+  isAboveRating(index: number): boolean {
+    return index > this.rating;
+  }
+
   ngOnInit() {}
 
+}
+
+enum COLORS {
+  GREY = "#E0E0E0",
+  GREEN = "#76FF03",
+  YELLOW = "#FFCA28",
+  RED = "#DD2C00"
 }
