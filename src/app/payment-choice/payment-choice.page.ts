0e7d82ad04d186b6f5daacf448418d2da4607c9e
Jan Martens <jan.niklas.martens@mni.thm.de>
2020-06-29 00:11:38 +0200

    - Add doucmentation - Refactor

--- src/app/payment-choice/payment-choice.page.ts
+++ src/app/payment-choice/payment-choice.page.ts
@@ -9,12 +9,16 @@ import {ShipmentService} from '../shared/shipment.service';
     templateUrl: './payment-choice.page.html',
     styleUrls: ['./payment-choice.page.scss'],
 })
+/**
+ * Payment choice page shows the three supported payment methods and navigates to payment
+ */
 export class PaymentChoicePage implements OnInit {
 
     @Input() shipment: Shipment;
     currentPaymentChoice: number;
 
     constructor(private router: Router, private toastController: ToastController, private route: ActivatedRoute, private shipmentService: ShipmentService) {
+       // method call collects passed data (shipment object) from previous page
         this.route.queryParams.subscribe(params => {
             if (this.router.getCurrentNavigation().extras.state) {
                 console.log(params);
@@ -28,7 +32,11 @@ export class PaymentChoicePage implements OnInit {
     ngOnInit() {
     }
 
-
+    /**
+     * Method which is called when one of the radio buttons is selected
+     * Updates value for currently selected radio button
+     * @param event contains value of selected radio button
+     */
     setPaymentChoice(event) {
         this.currentPaymentChoice = event.detail.value;
         switch (event.detail.value) {
@@ -46,6 +54,10 @@ export class PaymentChoicePage implements OnInit {
         console.log('radioGroupChange', this.currentPaymentChoice);
     }
 
+    /**
+     * Method for presenting a toast
+     * @param msg message that is displayed in the toast
+     */
     async presentToast(msg: string) {
         const toast = await this.toastController.create({
             message: msg,
@@ -54,50 +66,24 @@ export class PaymentChoicePage implements OnInit {
         toast.present();
     }
 
-
+    /**
+     * Payment method that updates the payment status of the shipment and continues with the selected choice or directs to the success page
+     */
     pay(){
         this.shipmentService.update(this.shipment.cargonaut, this.shipment.vehicle, this.shipment.passengerList, this.shipment.articleList, this.shipment.start, this.shipment.goal, this.shipment.date, this.shipment.startTime, this.shipment.length, this.shipment.height, this.shipment.weight, this.shipment.pricePerKg, this.shipment.seat, this.shipment.pricePerSeat, this.currentPaymentChoice, this.shipment.id);
 
     }
 
 
-    navigateToSearchResult() {
-        this.router.navigate(['/search-result']);
-    }
-
+    // navigation method to the login page
     navigateToLogin() {
         this.router.navigate(['/login']);
     }
 
+    // navigation method to the register page
     navigateToRegister() {
         this.router.navigate(['/register']);
     }
 
-    navigateToMangeVehicle() {
-        this.router.navigate(['/manage-vehicle']);
-    }
 
-    navigateToRouteSearch() {
-        this.router.navigate(['/route-search']);
-    }
-
-    navigateToTransportSearch() {
-        this.router.navigate(['/transport-search']);
-    }
-
-    navigateToProfile() {
-        this.router.navigate(['/profile']);
-    }
-
-    navigateToHome() {
-        this.router.navigate(['/home']);
-    }
-
-    navigateToImpressum(){
-        this.router.navigate(['/impressum']);
-    }
-
-    navigateToDelivery() {
-        this.router.navigate(['/delivery']);
-    }
 }
