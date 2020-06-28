a6960293551f3ca6ffee73dfb75934e5417412a8
Jan Martens <jan.niklas.martens@mni.thm.de>
2020-06-28 02:13:22 +0200

    - Implement query calls and data passing

--- src/app/payment-choice/payment-choice.page.ts
+++ src/app/payment-choice/payment-choice.page.ts
@@ -1,55 +1,79 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Shipment} from '../shared/shipment';
 import {Router} from '@angular/router';
+import {ToastController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-payment-choice',
-  templateUrl: './payment-choice.page.html',
-  styleUrls: ['./payment-choice.page.scss'],
+    selector: 'app-payment-choice',
+    templateUrl: './payment-choice.page.html',
+    styleUrls: ['./payment-choice.page.scss'],
 })
 export class PaymentChoicePage implements OnInit {
 
-  @Input() shipment: Shipment;
-  currentPaymentChoice: number;
-  constructor(private router: Router) { }
+    @Input() shipment: Shipment;
+    currentPaymentChoice: number;
 
-  ngOnInit() {
-  }
+    constructor(private router: Router, private toastController: ToastController) {
+    }
+
+    ngOnInit() {
+    }
 
 
-  setPaymentChoice(event){
-    this.currentPaymentChoice = event.detail.value;
-    console.log('radioGroupChange', this.currentPaymentChoice);
-  }
+    setPaymentChoice(event) {
+        this.currentPaymentChoice = event.detail.value;
+        switch (event.detail.value) {
+            case '0':
+                this.presentToast('Sie bezahlen mit Vorkasse');
+                break;
+            case '1':
+                this.presentToast('Sie bezahlen mit PayPal');
+                break;
+            case '2':
+                this.presentToast('Sie bezahlen Bar');
+                break;
 
-  navigateToSearchResult(){
-    this.router.navigate(['/search-result']);
-  }
+        }
+        console.log('radioGroupChange', this.currentPaymentChoice);
+    }
 
-  navigateToLogin(){
-    this.router.navigate(['/login']);
-  }
+    async presentToast(msg: string) {
+        const toast = await this.toastController.create({
+            message: msg,
+            duration: 2000
+        });
+        toast.present();
+    }
 
-  navigateToRegister(){
-    this.router.navigate(['/register']);
-  }
+    navigateToSearchResult() {
+        this.router.navigate(['/search-result']);
+    }
 
-  navigateToMangeVehicle(){
-    this.router.navigate(['/manage-vehicle']);
-  }
+    navigateToLogin() {
+        this.router.navigate(['/login']);
+    }
 
-  navigateToRouteSearch(){
-    this.router.navigate(['/route-search']);
-  }
+    navigateToRegister() {
+        this.router.navigate(['/register']);
+    }
 
-  navigateToTransportSearch(){
-    this.router.navigate(['/transport-search']);
-  }
-  navigateToProfile(){
-    this.router.navigate(['/profile']);
-  }
+    navigateToMangeVehicle() {
+        this.router.navigate(['/manage-vehicle']);
+    }
 
-  navigateToHome(){
-    this.router.navigate(['/home']);
-  }
+    navigateToRouteSearch() {
+        this.router.navigate(['/route-search']);
+    }
+
+    navigateToTransportSearch() {
+        this.router.navigate(['/transport-search']);
+    }
+
+    navigateToProfile() {
+        this.router.navigate(['/profile']);
+    }
+
+    navigateToHome() {
+        this.router.navigate(['/home']);
+    }
 }
